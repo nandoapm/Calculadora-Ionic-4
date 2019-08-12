1e7828973048531ea7bc704e068cfb5b28f6ae53
Fernando Melo <nandoapm@gmail.com>
2019-08-12 13:23:34 -0300

    finalizando calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  numeroAtual = '0';
+  ultimoNumero = '0';
+
+  ultimoOperador = 'x';
+  novoCalculo = true;
+  grupoNumeros = [
+    [7, 8, 9, '+'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '*'],
+    ['c', 0, '=', '/']
+  ];
+
+  calculo(operadores: any) {
+    console.log(operadores);
+
+    if (isNumber(operadores)) {
+      console.log('número');
+      if (this.novoCalculo) {
+        this.numeroAtual = '' + operadores;
+      } else {
+        this.numeroAtual += '' + operadores;
+      }
+      this.novoCalculo = false;
+    } else if (operadores === 'c') {
+        this.numeroAtual = '0';
+        this.novoCalculo = true;
+    } else if (operadores === '=') {
+        if (this.ultimoOperador === '*') {
+          this.numeroAtual = '' + (parseInt (this.ultimoNumero) * parseInt(this.numeroAtual));
+        } else if (this.ultimoOperador === '-') {
+          this.numeroAtual = '' + (parseInt(this.ultimoNumero) - parseInt(this.numeroAtual));
+        } else if (this.ultimoOperador === '+') {
+          this.numeroAtual = '' + (parseInt(this.ultimoNumero) + parseInt(this.numeroAtual));
+        } else if (this.ultimoOperador === '/') {
+          this.numeroAtual = '' + (parseInt(this.ultimoNumero) / parseInt(this.numeroAtual));
+        }
+    } else { // operador
+      this.novoCalculo = true;
+      this.ultimoNumero = this.numeroAtual;
+      this.ultimoOperador = operadores;
+    }
+  }
 }
